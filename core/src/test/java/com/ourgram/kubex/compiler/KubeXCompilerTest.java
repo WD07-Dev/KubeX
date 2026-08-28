b37f99a8e5d1afdb821d17302cadfc69b85ea23f
wheedo07 <wheedo07@ourgram.co.kr>
2026-08-28 13:45:31 +0900

    수정

--- core/src/test/java/com/ourgram/kubex/compiler/KubeXCompilerTest.java
+++ core/src/test/java/com/ourgram/kubex/compiler/KubeXCompilerTest.java
@@ -15,7 +15,6 @@ import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
